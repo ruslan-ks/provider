19c73e7c4798798eb658c79ea369c1abb4dea519
Ruslan Kostyuk <rsln.kostuk@gmail.com>
2022-09-09 00:50:20 +0300

    Add AppListener class

---
+++ src/main/java/com/provider/controller/listeners/AppListener.java
@@ -0,0 +1,45 @@
+package com.provider.controller.listeners;
+
+import com.provider.constants.attributes.AppAttributes;
+import com.provider.localization.LanguageInfo;
+import com.provider.localization.MapBasedLanguageLocaleInfo;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Sets application initial attributes used by its parts
+ */
+@WebListener
+public class AppListener implements ServletContextListener {
+    private final Logger logger = LoggerFactory.getLogger(AppListener.class);
+
+    public AppListener() {}
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        setAppScopeAttributes(servletContextEvent.getServletContext());
+        logger.trace("Application context initialized");
+    }
+
+    private void setAppScopeAttributes(@NotNull ServletContext servletContext) {
+        setAppLanguageInfoAttribute(servletContext);
+    }
+
+    private void setAppLanguageInfoAttribute(@NotNull ServletContext servletContext) {
+        final Map<String, Locale> languageLocaleMap = Map.of("en", Locale.ENGLISH,
+                "uk", new Locale("uk", "UA"));
+        final LanguageInfo languageInfo = MapBasedLanguageLocaleInfo.newInstance(languageLocaleMap);
+        servletContext.setAttribute(AppAttributes.LANGUAGE_INFO, languageInfo);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        /* This method is called when the servlet Context is undeployed or Application Server shuts down. */
+    }
+}
