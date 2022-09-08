afe34b64df081c5e6755b0db9387492e2c8cc628
Ruslan Kostyuk <rsln.kostuk@gmail.com>
2022-09-09 00:49:51 +0300

    Add LanguageInfo interface and implementation

---
+++ src/main/java/com/provider/localization/MapBasedLanguageLocaleInfo.java
@@ -0,0 +1,49 @@
+package com.provider.localization;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * Contains languages and corresponding locales. Based on Map where keys are languages and values are locales.
+ */
+public class MapBasedLanguageLocaleInfo implements LanguageInfo {
+    private final Map<String, Locale> languageLocaleMap;
+
+    private MapBasedLanguageLocaleInfo(@NotNull Map<String, Locale> languageLocaleMap) {
+        this.languageLocaleMap = Map.copyOf(languageLocaleMap);
+    }
+
+    /**
+     * Creates new instance
+     * @param languageLocaleMap a map of supported languages and corresponding locales
+     * @return new instance
+     */
+    public static @NotNull MapBasedLanguageLocaleInfo newInstance(@NotNull Map<String, Locale> languageLocaleMap) {
+        return new MapBasedLanguageLocaleInfo(languageLocaleMap);
+    }
+
+    @Override
+    public @NotNull Set<String> getSupportedLanguages() {
+        return languageLocaleMap.keySet();
+    }
+
+    @Override
+    public @NotNull Locale getLocale(@NotNull String language) {
+        if (!languageLocaleMap.containsKey(language)) {
+            throw new IllegalArgumentException("Language '" + language + "' not supported");
+        }
+        return languageLocaleMap.get(language);
+    }
+
+    @Override
+    public @NotNull String getDefaultLanguage() {
+        if (languageLocaleMap.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return languageLocaleMap.keySet().iterator().next();
+    }
+}
