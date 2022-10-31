db46762d429b002e532f299edc2772d16d70ced7
Ruslan Kostyuk <rsln.kostuk@gmail.com>
2022-10-31 22:58:24 +0200

    Add ukrainian specific letters support

--- src/main/java/com/provider/constants/Regex.java
+++ src/main/java/com/provider/constants/Regex.java
@@ -3,11 +3,11 @@ package com.provider.constants;
 public class Regex {
     private Regex() {}
 
-    public static final String REGULAR_TEXT = "^[a-zA-Zа-яА-Я0-9 .,;!?-]+$";
+    public static final String REGULAR_TEXT = "^[a-zA-Zа-яА-ЯіїґєІЇҐЄ0-9 .,;!?-]+$";
 
     public static final String FILE_NAME = "^[a-zA-Z0-9_.]+$";
 
-    public static final String NAME = "^[A-Za-z0-9-]+$";
+    public static final String NAME = "^[a-zA-Zа-яА-ЯіїґєІЇҐЄ0-9-]+$";
 
     public static final String PHONE = "^([1-9][0-9])?[0-9]{6,10}$";
 
