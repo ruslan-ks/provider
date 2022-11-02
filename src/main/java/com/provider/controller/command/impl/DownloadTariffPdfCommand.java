1813c4f609cbdf9b7e61cac4296a88f7271a7223
Ruslan Kostyuk <rsln.kostuk@gmail.com>
2022-11-02 17:25:58 +0200

    Refactor DownloadTariffPdfCommand

--- src/main/java/com/provider/controller/command/impl/DownloadTariffPdfCommand.java
+++ src/main/java/com/provider/controller/command/impl/DownloadTariffPdfCommand.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Optional;
 
 public class DownloadTariffPdfCommand extends FrontCommand {
@@ -38,43 +39,47 @@ public class DownloadTariffPdfCommand extends FrontCommand {
         final String tariffIdParam = getRequiredParam(TariffParams.ID);
         final int tariffId = CommandUtil.parseIntParam(tariffIdParam);
 
-        // TODO: refactor
         final String locale = "en";
 
         final TariffService tariffService = serviceFactory.getTariffService();
-        final Optional<TariffDto> tariffOptional = tariffService.findTariffFullInfoById(tariffId, locale);
+        final Optional<TariffDto> tariffDtoOptional = tariffService.findTariffFullInfoById(tariffId, locale);
 
-        if (tariffOptional.isPresent()) {
-            final Tariff tariff = tariffOptional.get().getTariff();
+        if (tariffDtoOptional.isPresent()) {
             response.setContentType("application/pdf");
-            final PdfWriter pdfWriter = new PdfWriter(response.getOutputStream());
-            final PdfDocument pdfDocument = new PdfDocument(pdfWriter);
-            pdfDocument.addNewPage();
-
-            try (var document = new Document(pdfDocument)) {
-                final var titleParagraph = new Paragraph(tariff.getTitle());
-                document.add(titleParagraph);
-
-                final var priceParagraph = new Paragraph("Price: $" + tariff.getUsdPrice());
-                document.add(priceParagraph);
-
-                final TariffDuration duration = tariffOptional.get().getDuration();
-                final var durationParagraph = new Paragraph("Duration: " + duration.getMonths()
-                        + " months, " + duration.getMinutes() + " minutes");
-                document.add(durationParagraph);
-
-                final var descriptionParagraph = new Paragraph(tariff.getDescription());
-                document.add(descriptionParagraph);
-
-                final var list = new List();
-                for (var service : tariffOptional.get().getServices()) {
-                    list.add(service.getName() + ": " + service.getDescription());
-                }
-                document.add(list);
+            try (var os = response.getOutputStream()) {
+                writeTariffPdf(tariffDtoOptional.get(), os);
             }
-            logger.debug("PDF document written");
         }
-
         return CommandResult.NO_VIEW;
+    }
+
+    private void writeTariffPdf(@NotNull TariffDto tariffDto, @NotNull OutputStream os) {
+        final Tariff tariff = tariffDto.getTariff();
+        final PdfWriter pdfWriter = new PdfWriter(os);
+        final PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+        pdfDocument.addNewPage();
+
+        try (var document = new Document(pdfDocument)) {
+            final var titleParagraph = new Paragraph(tariff.getTitle());
+            document.add(titleParagraph);
+
+            final var priceParagraph = new Paragraph("Price: $" + tariff.getUsdPrice());
+            document.add(priceParagraph);
+
+            final TariffDuration duration = tariffDto.getDuration();
+            final var durationParagraph = new Paragraph("Duration: " + duration.getMonths()
+                    + " months, " + duration.getMinutes() + " minutes");
+            document.add(durationParagraph);
+
+            final var descriptionParagraph = new Paragraph(tariff.getDescription());
+            document.add(descriptionParagraph);
+
+            final var list = new List();
+            for (var service : tariffDto.getServices()) {
+                list.add(service.getName() + ": " + service.getDescription());
+            }
+            document.add(list);
+        }
+        logger.debug("PDF document written! TariffDto: {}", tariffDto);
     }
 }
